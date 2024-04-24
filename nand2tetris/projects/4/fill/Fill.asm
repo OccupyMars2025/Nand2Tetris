86b67c7a638c1fe9b6c751a73d85a23d9664fe5a
OccupyMars2025 <3119002196@qq.com>
2024-04-24 21:55:59 +0800

    complete project 4: Machine Language

--- nand2tetris/projects/4/fill/Fill.asm
+++ nand2tetris/projects/4/fill/Fill.asm
@@ -8,4 +8,62 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// pseudo code
+// while(true) {
+//     if((*R0) >= 8192) {// 
+//        (*R0) = 0;
+//     }
+//     if((*KBD) != 0) {// 
+//        *(SCREEN + (*R0)) = -1;
+//     } else {
+//         *(SCREEN + (*R0)) = 0;
+//     }
+//     (*R0) = (*R0) + 1;
+// }
+
+
+// use R0 to index the array of 16-bit words
+(LOOP_FOREVER)
+    // if((*R0) >= 8192)
+    @R0
+    D=M
+    @8192
+    D=D-A
+    @SCREEN_INDEX
+    D;JLT
+    // (*R0) = 0
+    @R0
+    M=0
+
+    (SCREEN_INDEX)
+        @KBD
+        D=M
+        @NO_KEYBOAD_PRESSED
+        D;JEQ
+        // if((*KBD) != 0)
+        //  *(SCREEN + (*R0)) = -1;
+        @R0
+        D=M
+        @SCREEN
+        A=D+A
+        M=-1
+        @INCREASE_R0
+        0;JMP
+
+        // else
+        //  *(SCREEN + (*R0)) = 0;
+        (NO_KEYBOAD_PRESSED)
+            @R0
+            D=M
+            @SCREEN
+            A=D+A
+            M=0
+
+    (INCREASE_R0)
+        // (*R0) = (*R0) + 1;
+        @R0
+        M=M+1
+
+    @LOOP_FOREVER
+    0;JMP
+
--- nand2tetris/projects/4/mult/Mult.asm
+++ nand2tetris/projects/4/mult/Mult.asm
@@ -7,4 +7,41 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// pseudo code:
+// R2=0
+// LOOP:
+//     if(R1 == 0) goto END
+//     R2 += R0
+//     R1 -= 1
+//     goto LOOP
+// END:
+
+// modify R1 in place, reduce R1 one by one
+@R2
+M=0
+(LOOP)
+    // if(R1 == 0) goto END
+    @R1
+    D=M
+    @END
+    D;JEQ
+
+    // R2 += R0
+    @R0
+    D=M
+    @R2
+    M=D+M
+
+    // R1 -= 1
+    @R1
+    M=M-1
+
+    // goto LOOP
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
+
+
