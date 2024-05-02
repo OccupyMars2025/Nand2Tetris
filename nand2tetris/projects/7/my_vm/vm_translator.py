995add52aa3ca526672c081ba977c2c04647ab02
OccupyMars2025 <3119002196@qq.com>
2024-05-02 17:50:36 +0800

    project 7: pass the test of BasicTest

--- nand2tetris/projects/7/my_vm/vm_translator.py
+++ nand2tetris/projects/7/my_vm/vm_translator.py
@@ -29,6 +29,7 @@ class VMTranslator:
 if __name__ == '__main__':
     # python -m my_vm.vm_translator  ./StackArithmetic/SimpleAdd/SimpleAdd.vm
     # python -m my_vm.vm_translator  ./StackArithmetic/StackTest/StackTest.vm
+    # python -m my_vm.vm_translator  ./MemoryAccess/BasicTest/BasicTest.vm
     import sys
     if len(sys.argv) != 2:
         print('Please provide a VM file.')
